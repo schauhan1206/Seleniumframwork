911a4cdc184790e5fb37f96101d4449f9b046264
seemal <schauhan1206@gmail.com>
2019-01-02 16:26:28 -0500

    push

--- SeleniumFramework/src/test/java/BrowserTest.java
+++ SeleniumFramework/src/test/java/BrowserTest.java
@@ -1,27 +1,40 @@
 import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BrowserTest {
-	
+
 	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\seemal\\git\\Seleniumframwork\\SeleniumFramework\\Drivers\\newlatestchromer\\chromedriver.exe");
 		
-		String projectPath = System.getProperty("user.dir");
-		System.out.println("projectPath :" +projectPath);
-		
-		
-		
-		System.setProperty("webdriver.gecko.driver","C:\\Users\\seemal\\eclipse-workspace\\SeleniumFramework\\Drivers\\geckodriver\\geckodriver.exe");
-		WebDriver driver = new FirefoxDriver();
-		
-		
-	
-		driver.get("https://www.seleniumhq.org/");
+		//WebDriver driver = new ChromeDriver();
+		//driver.get("https://www.seleniumhq.org/");
+		//driver.close();
 		
 		
 		
 		
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\seemal\\git\\Seleniumframwork\\SeleniumFramework\\Drivers\\geckodriver\\geckodriver.exe");
+             WebDriver  driver = new FirefoxDriver();
+             
+             
+             
+             
+             
+             driver.get("https://www.seleniumhq.org/");
+             
+             
+             
+             
+             
+             
+             
+		  
 		
 		
+		
+		
+
 	}
 
 }
